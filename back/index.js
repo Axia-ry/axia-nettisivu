371ec939c35f207a8845e1251aef54a3c307cb42
jon-erikklint <jonerik.klint@gmail.com>
2018-07-21 12:54:50 +0300

    back alku toimii

--- back/index.js
+++ back/index.js
@@ -1,20 +1,21 @@
 const http = require('http')
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 
 const config = require('./utils/config')
-const getAuthorizationToken = require('./utils/middleware')
+//const getAuthorizationToken = require('./utils/middleware')
 
-mongoose.connect(config.mongoUrl)
-mongoose.Promise = global.Promise
+//mongoose.connect(config.mongoUrl)
+//mongoose.Promise = global.Promise
 
+app.use(express.static('build'))
 app.use(cors())
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 
-app.use(getAuthorizationToken)
+//app.use(getAuthorizationToken)
 
 //app.use('/api/blogs', blogsRouter)
 
@@ -24,9 +25,9 @@ server.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
 })
 
-server.on('close', () => {
+/*server.on('close', () => {
   mongoose.connection.close()
-})
+})*/
 
 module.exports = {
   app, server
--- back/utils/config.js
+++ back/utils/config.js
@@ -5,10 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
 let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URL
 
-if (process.env.NODE_ENV === 'test') {
+/*if (process.env.NODE_ENV === 'test') {
   port = process.env.TEST_PORT
   mongoUrl = process.env.TEST_MONGODB_URL
-}
+}*/
 
 module.exports = {
   mongoUrl,
