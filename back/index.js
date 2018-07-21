51187fdb7f2ffa136523334e09097728c6bcb5a3
jon-erikklint <jonerik.klint@gmail.com>
2018-07-21 12:47:15 +0300

    backin alku

---
+++ back/index.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+const getAuthorizationToken = require('./utils/middleware')
+
+mongoose.connect(config.mongoUrl)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(getAuthorizationToken)
+
+//app.use('/api/blogs', blogsRouter)
+
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
+})
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
