9ce38418beb09277b518cc0f6fc8e64f2faaa2ce
jon-erikklint <jonerik.klint@gmail.com>
2018-10-07 22:06:05 +0300

    muutokset

--- back/controllers/teams.js
+++ back/controllers/teams.js
@@ -3,26 +3,20 @@ const teamsRouter = require('express').Router()
 teamsRouter.get('/', (request, response) => {
   response.json([
     {
-      Name: 'AxiA 04 kilpajoukkue', 
+      Name: 'AxiA 04 joukkue', 
       Coach: 'Vastuuvalmentaja Marco Uusimaa, jojo Ismo Liljendahl', 
-      PracticeTime: 'Ma klo 18-20 Jokelan koulukeskus, Ke klo 17.30-19 Kellokosken Woima -sali, La klo 9-11 Kellokosken Woima -sali', 
-      ImageLink: '/images/logo.gif'
+      PracticeTime: 'Ma klo 18-20 Jokelan koulukeskus, Ke klo 17.30-19 Kellokosken Woima -sali, La klo 9-11 Kellokosken Woima -sali, Su klo 18-20 Isät / pojat', 
+      ImageLink: '/images/axia_04.jpg'
     }, 
     {
-      Name: 'Axia 08 harrastejoukkue', 
-      Coach: 'Vastuuvalmentaja Tuomas Laukka, valmentajat Tuomas Tella, Miko Kutinlahti, jojo Laura Raitala', 
-      PracticeTime: 'To klo 17-18 Hyökkälän ya, Pe klo 18-19 Kellokosken Woima -sali, Su klo 13.30 -15 Kellokosken Woima -sali', 
-      ImageLink: '/images/logo.gif'
+      Name: 'Axia 08 ja 09 joukkueet', 
+      Coach: 'Vastuuvalmentaja Tuomas Laukka 08, Vastuuvalmentaja Teemu Kiuttu 09, Apuvalmentajat Tuomas Tella, Jussi Niemi, Jojo Laura Raitala , Nikke Holopainen', 
+      PracticeTime: 'Ke klo 16.30-17.30 Kellokosken Woima -sali, Pe klo 17.30 -19 Kellokosken Woima -sali, Su klo 12 -13.30  Kellokosken Woima -sali', 
+      ImageLink: '/images/axia_09.jpg'
     },
     {
-      Name: 'AxiA 09 kilpajoukkue', 
-      Coach: 'Vastuuvalmentaja Teemu Kiuttu , jojo Nikke Holopainen', 
-      PracticeTime: 'Ko klo 17-18 Kellokosken Woima -sali, Pe klo 17-18 Kellokosken Woima -sali, Su klo 12 -13.30  Kellokosken Woima -sali', 
-      ImageLink: '/images/logo.gif'
-    },
-    {
-      Name: 'Miesten harraste, isät-pojat vuoro',  
-      PracticeTime: 'Su klo 18-20 Kellokosken Woima -sali', 
+      Name: 'AxiA kerhot (ikäluokat 2010-2011)',  
+      PracticeTime: 'Pe klo 16.30 - 17.30 Kellokosken Woima -sali, ti klo 16.30 -17.30 Paijalan koulu', 
       ImageLink: '/images/logo.gif'
     }
   ])
--- front/src/components/page/SeuraPage.js
+++ front/src/components/page/SeuraPage.js
@@ -6,6 +6,12 @@ class SeuraPage extends Component{
   render() {
     return (
       <div>
+        <h2>Seuran tiedot</h2>
+        <ul>
+          <li className="seura-link"><a href="https://drive.google.com/open?id=1mLNj-DS2agD7tmWr--5jzrxiFxqldKrw">Hallinnon tehtävät</a></li>
+          <li className="seura-link"><a href="https://drive.google.com/open?id=1E8cnGc3JHWP5jDI7oiVMWd733PLdnhQR">Joukkuepelisäännöt</a></li>
+          <li className="seura-link"><a href="https://drive.google.com/open?id=1MujGFuvrNDzVPTzz5WZYCeui_Oj1c7IV">Säännöt</a></li>
+        </ul>
         <Text location='seuraText'/>
       </div>
     )
